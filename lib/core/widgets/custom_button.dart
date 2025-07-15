fcb9c5992dcdba31df07c3a316c4b6eef6e2c37d
taejun <ptj01036251@gmail.com>
2025-07-15 15:35:05 +0900

    feat: CustomButton 개발

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomButton extends StatelessWidget {
+  Color? backgroundColor;
+  VoidCallback? onPressed;
+  final String text;
+
+  CustomButton({
+    super.key,
+    required this.text,
+    this.backgroundColor,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(320.w, 52.h),
+        backgroundColor: backgroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.r),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16.sp,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
