e4822b75cdafc25c7bfc0f5a2eb8b35a8b5dd4f3
taejun <ptj01036251@gmail.com>
2025-07-15 15:34:53 +0900

    feat: 이미지 경로 추가

---
+++ lib/core/images.dart
@@ -0,0 +1,6 @@
+class Images {
+  static String logo = 'assets/images/logo.png';
+
+  static String eyeOn = 'assets/images/icons/eye-on.png';
+  static String eyeOff = 'assets/images/icons/eye-off.png';
+}
